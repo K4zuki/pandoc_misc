c282385b1dfdcff292d3062503d2aa6f5ee321bf
K4zuki <k.yamamoto.08136891@gmail.com>
2016-01-22 15:05:28 +0900

    new options x(y)min/x(y)max/x(y)label/x(y)suffix/x(y)unit

--- csv2png.py
+++ csv2png.py
@@ -9,14 +9,26 @@ import csv
 from matplotlib.font_manager import FontProperties
 fontP = FontProperties()
 fontP.set_size('small')
+fontP.set_family('Times New Roman')
 
 import argparse
 class MyParser(object):
     def __init__(self):
-        self._parser = argparse.ArgumentParser(description = """take screenshot from Tek MSO/DPO/MDO4000 Oscilloscope,
-        will *Not* control instuments other than Oscillo
-        """)
-        self._parser.add_argument('--file', '-F', help = 'inputcsv file name', default = 'default.csv')
+        self._parser = argparse.ArgumentParser(description = "")
+        self._parser.add_argument('--file', '-F', help = 'input csv file name', default = 'default.csv')
+
+        self._parser.add_argument('--xmin', default = 0)
+        self._parser.add_argument('--xmax', default = 1000)
+        self._parser.add_argument('--xlabel', default = "I")
+        self._parser.add_argument('--xsuffix', default = "in")
+        self._parser.add_argument('--xunit', default = "mA")
+
+        self._parser.add_argument('--ymin', default = 2.5)
+        self._parser.add_argument('--ymax', default = 5.5)
+        self._parser.add_argument('--ylabel', default = "V")
+        self._parser.add_argument('--ysuffix', default = "out")
+        self._parser.add_argument('--yunit', default = "V")
+
 #        self._parser.add_argument('--input', '-I', help = 'input voltage', default = 3.6)
         self._parser.add_argument('--output', '-O', help = 'output file name', default = 'default.png')
 #        self._parser.add_argument('--device', '-D', help = 'device number', default = 1)
@@ -27,6 +39,18 @@ class MyParser(object):
 parser = MyParser()
 _file = parser.args.file
 _output = parser.args.output
+
+_xmin =     float(parser.args.xmin)
+_xmax =     float(parser.args.xmax)
+_xlabel =   parser.args.xlabel
+_xsuffix =  parser.args.xsuffix
+_xunit =    parser.args.xunit
+
+_ymin =     float(parser.args.ymin)
+_ymax =     float(parser.args.ymax)
+_ylabel =   parser.args.ylabel
+_ysuffix =  parser.args.ysuffix
+_yunit =    parser.args.yunit
 
 read = csv.reader(open(_file,'r'), delimiter=',',quotechar='"')
 title = []
@@ -40,21 +64,22 @@ for row in read:
 data.reverse()
 x = data.pop()
 data.reverse()
-for hoge in range(len(x)):
-    x[hoge] = float(x[hoge])
+for i in range(len(x)):
+    x[i] = float(x[i])
 title.reverse()
 plt.title(title.pop())
 for y in data:
-    for hoge in range(len(y)):
-        y[hoge] = float(y[hoge])
+    for j in range(len(y)):
+        y[j] = float(y[j])
 #    print y
     plt.plot(x, y, 'o-', label = title.pop())
-
 plt.legend( prop = fontP, loc = 'upper center', ncol=2)
-plt.xlabel("$I_{out}$[mA]")
-plt.ylabel("$V_{in}$[V]")
-plt.xlim([0,1000])
-plt.ylim([2.5, 5.5])
+#plt.xlabel("$I_{out}$[mA]")
+plt.xlabel("$"+_xlabel+"_{"+_xsuffix+"}$["+_xunit+"]")
+plt.xlim([_xmin,_xmax])
+#plt.ylabel("$V_{in}$[V]")
+plt.ylabel("$"+_ylabel+"_{"+_ysuffix+"}$["+_yunit+"]")
+plt.ylim([_ymin, _ymax])
 plt.grid()
 plt.show()
 plt.savefig(_output)
