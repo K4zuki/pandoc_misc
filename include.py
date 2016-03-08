6aa2951cf6a07334046f5f8229644827b21b96ff
K4zuki <k.yamamoto.08136891@gmail.com>
2016-03-08 16:13:17 +0900

    add -B/--basedir option

--- include.py
+++ include.py
@@ -9,19 +9,22 @@ class MyParser(object):
         self._parser = argparse.ArgumentParser(description = "cat some.txt| python include.py --out out_f.md")
         self._parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
         self._parser.add_argument('--out', '-O', help = 'output markdown file', default = "table.md")
+        self._parser.add_argument('--basedir','-B', help = 'base directry of output', default = "./")
         self.args = self._parser.parse_args(namespace=self)
+
 
 parser = MyParser()
 _file = parser.args.infile
 _output = parser.args.out
+_basedir = parser.args.basedir
 
 include = re.compile("`([^`]+)`\{.include}")
 stripped = re.sub("<!--[\s\S]*?-->", "", _file.read())
-output = open(_output,'wb')
+output = open(_basedir+"/"+_output,'wb')
 
 for line in stripped.split("\n"):
     if include.search(line):
         input_file = include.search(line).groups()[0]
-        file_contents = open(input_file, "rb").read()
+        file_contents = open(_basedir+"/"+input_file, "rb").read()
         line = include.sub(line, file_contents)
     output.write(line+"\n")
