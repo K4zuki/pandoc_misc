e4a5976f0685fd4a979b163bcda3f58d8aa3501a
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2016-04-24 02:00:22 +0900

    functioned and recursive called

--- include.py
+++ include.py
@@ -1,40 +1,65 @@
 #-*- coding: utf-8 -*-
 #!/usr/bin/env python27
 
-import re
-import sys
-import argparse
-from file2listingtable import file2listingtable as f2l
+def include(file, basename = "./"):
+    _include = re.compile("`([^`]+)`\{.include}")
+    listing = re.compile("`([^`]+)`\{.listingtable\ (\.[^`\.]+)}")
+    stripped = re.sub("<!--[\s\S]*?-->", "", open(file, "rb").read())
+    output = []
 
-class MyParser(object):
-    def __init__(self):
-        self._parser = argparse.ArgumentParser(description = "cat some.txt| python include.py --out out_f.md")
-        self._parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-        self._parser.add_argument('--out', '-O', help = 'output markdown file', default = "table.md")
-        self._parser.add_argument('--basedir','-B', help = 'base directry of output', default = "./")
-        self.args = self._parser.parse_args(namespace=self)
+    for line in stripped.split("\n"):
+        if _include.search(line):
+            print file+" include",
+            input_file = _include.search(line).groups()[0]
+            print input_file
+            # file_contents = open(input_file, "rb").read()
+            line = _include.sub(line, include(basename+input_file))
+        if listing.search(line):
+            print file+" lisingtable",
+            input_file = listing.search(line).groups()[0]
+            filetype = listing.search(line).groups()[1]
+            print filetype
+            convert = f2l(input_file, filetype)
+            line = listing.sub(line, convert)
+        output.append(line)
+    return "\n".join(output)
+
+if __name__ == '__main__':
+    import re
+    import sys
+    import argparse
+    from file2listingtable import file2listingtable as f2l
+
+    class MyParser(object):
+        def __init__(self):
+            self._parser = argparse.ArgumentParser(description = "cat some.txt| python include.py --out out_f.md")
+            self._parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+            self._parser.add_argument('--out', '-O', help = 'output markdown file', default = "table.md")
+            self._parser.add_argument('--basedir','-B', help = 'base directry of output', default = "./")
+            self.args = self._parser.parse_args(namespace=self)
 
 
-parser = MyParser()
-_file = parser.args.infile
-_output = parser.args.out
-_basedir = parser.args.basedir
+    parser = MyParser()
+    _file = parser.args.infile
+    _output = parser.args.out
+    _basedir = parser.args.basedir
 
-include = re.compile("`([^`]+)`\{.include}")
-listing = re.compile("`([^`]+)`\{.listingtable\ (\.[^`\.]+)}")
-stripped = re.sub("<!--[\s\S]*?-->", "", _file.read())
-output = open(_basedir+"/"+_output,'wb')
+    _include = re.compile("`([^`]+)`\{.include}")
+    listing = re.compile("`([^`]+)`\{.listingtable\ (\.[^`\.]+)}")
+    stripped = re.sub("<!--[\s\S]*?-->", "", _file.read())
+    output = open(_basedir+"/"+_output,'wb')
 
-for line in stripped.split("\n"):
-    if include.search(line):
-        input_file = include.search(line).groups()[0]
-        file_contents = open(_basedir+"/"+input_file, "rb").read()
-        line = include.sub(line, file_contents)
-    elif listing.search(line):
-        print "!"
-        input_file = listing.search(line).groups()[0]
-        filetype = listing.search(line).groups()[1]
-        print filetype
-        convert = f2l(input_file, filetype)
-        line = listing.sub(line, convert)
-    output.write(line+"\n")
+    for line in stripped.split("\n"):
+        if _include.search(line):
+            print "main include",
+            input_file = _include.search(line).groups()[0]
+            print input_file
+            line = _include.sub(line, include(input_file, _basedir))
+        if listing.search(line):
+            print "main lisingtable",
+            input_file = listing.search(line).groups()[0]
+            filetype = listing.search(line).groups()[1]
+            print filetype
+            convert = f2l(input_file, filetype)
+            line = listing.sub(line, convert)
+        output.write(line+"\n")
