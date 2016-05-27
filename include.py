7abbcbb357fd15628e693af73c7b39f177d41279
K4zuki <k.yamamoto.08136891@gmail.com>
2016-05-27 11:50:29 +0900

    better look

--- include.py
+++ include.py
@@ -8,9 +8,12 @@ modesel = {
 }
 
 def include(file, basename = "./", mode = "none"):
-    _include = re.compile("`([^`]+)`\{.include}") # regex filter to find out include statement
-    listing = re.compile("`([^`]+)`\{.listingtable\ (\.[^`\.]+)}") # regex filter to find out listing statement
-    stripped = re.sub("<!--[\s\S]*?-->", "", open(file, "rb").read()) # regex filter to remove markdown comment
+    # regex filter to find out include statement
+    _include = re.compile("`([^`]+)`\{.include}")
+    # regex filter to find out listing statement
+    listing = re.compile("`([^`]+)`\{.listingtable\ (\.[^`\.]+)}")
+    # regex filter to remove markdown comment
+    stripped = re.sub("<!--[\s\S]*?-->", "", open(file, "rb").read())
 
     _mode = modesel[mode.upper()]
     # print _mode
@@ -42,11 +45,16 @@ if __name__ == '__main__':
 
     class MyParser(object):
         def __init__(self):
-            self._parser = argparse.ArgumentParser(description = "cat some.txt| python include.py --out out_f.md")
-            self._parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-            self._parser.add_argument('--out', '-O', help = 'output markdown file', default = "table.md")
-            self._parser.add_argument('--basedir','-B', help = 'base directry of output', default = "./")
-            self._parser.add_argument('--mode','-M', help = 'TeX or DOCX output', default = "none")
+            self._parser = argparse.ArgumentParser(description =
+                "cat some.txt| python include.py --out out_f.md")
+            self._parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
+                default=sys.stdin)
+            self._parser.add_argument('--out', '-O', help = 'output markdown file',
+                default = "table.md")
+            self._parser.add_argument('--basedir','-B', help = 'base directry of output',
+                default = "./")
+            self._parser.add_argument('--mode','-M', help = 'TeX or DOCX output',
+                default = "none")
             self.args = self._parser.parse_args(namespace=self)
 
     parser = MyParser()
@@ -57,12 +65,18 @@ if __name__ == '__main__':
     __mode = modesel[_mode.upper()]
     # print _mode
 
-    _include = re.compile("`([^`]+)`\{.include}") # regex filter to find out include statement
-    listing = re.compile("`([^`]+)`\{.listingtable\ (\.[^`\.]+)}") # regex filter to find out listing statement
-    stripped = re.sub("<!--[\s\S]*?-->", "", _file.read()) # regex filter to remove markdown comment
-    tblcaption = re.compile("(Table:)([\*_\ ]*)(.[^\{\}\*_]*)([\*_\ ]*).*\{\ *#(tbl:.[^\*\n]*[^\ ])\ *\}") # regex filter to find out table caption
-    # figcaption = re.compile("(Figure:)([\*_\ ]*)(.[^\{\}\*_]*)([\*_\ ]*).*\{\ *#(fig:.[^\*\n]*[^\ ])\ *\}") # regex filter to find out figure caption
-    # lstcaption = re.compile("(List:)([\*_\ ]*)(.[^\{\}\*_]*)([\*_\ ]*).*\{\ *#(lst:.[^\*\n]*[^\ ])\ *\}") # regex filter to find out listing caption
+    # regex filter to find out include statement
+    _include = re.compile("`([^`]+)`\{.include}")
+
+    # regex filter to find out listing statement
+    listing = re.compile("`([^`]+)`\{.listingtable\ (\.[^`\.]+)}")
+
+    # regex filter to remove markdown comment
+    stripped = re.sub("<!--[\s\S]*?-->", "", _file.read())
+
+    # regex filter to find out table caption
+    tblcaption = re.compile(
+        "(Table:)([\*_\ ]*)(.[^\{\}\*_]*)([\*_\ ]*).*\{\ *#(tbl:.[^\*\n]*[^\ ])\ *\}")
     output = open(_basedir + "/" + _output,'wb')
 
     for line in stripped.split("\n"):
@@ -84,13 +98,4 @@ if __name__ == '__main__':
                 link =      tblcaption.search(line).groups()[4]
                 line = "TC \"[@"+link+"] "+caption+"\" `\l` 5\n\n"+line
                 print link
-        # if figcaption.search(line):
-        #     caption =   figcaption.search(line).groups()[2]
-        #     link =      figcaption.search(line).groups()[4]
-        #     print "TC \"[@"+link+"] "+caption+"\" `\l` 6\n"+line
-        # if lstcaption.search(line):
-        #     caption =   lstcaption.search(line).groups()[2]
-        #     link =      lstcaption.search(line).groups()[4]
-        #     # line = listing.sub(line, convert)
-        #     print "TC \"[@"+link+"] "+caption+"\" `\l` 7\n"+line
         output.write(line+"\n")
