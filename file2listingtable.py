4e07799a975044c4f1c73c467e106ef6077e0048
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2016-08-11 20:01:03 +0900

    return error message if fail to read listing file

--- file2listingtable.py
+++ file2listingtable.py
@@ -32,65 +32,68 @@ def file2listingtable(file = "Makefile", type = ".makefile", docx = False, tex =
         width = len(_list[i])
         widthlist.append( width )
 
-    read = open(_file,"r")
-    _read = list(read)
-    # print _read
-
-    height = len(_read) #number of lines in list
-    # print height
-
-    for i in range(height):
-        swap = _read[i].rstrip('\n').replace("\t","    ")
-        width = len(swap )
-        widthlist.append( width )
-    _maxwidth = max(widthlist)
-    # print _maxwidth
-
-    hline = "+" + "".ljust(_maxwidth,"-") + "+"
-    # print hline
-
-    headers = []
-    for statement in _list:
-        # print statement
-        header = "|" + statement.ljust(_maxwidth) + "|"
-        headers.append(header)
-    # for i in range(len(_list)):
-    #     # print _list[i]
-    #     header = "|" + _list[i].ljust(_maxwidth) + "|"
-    #     headers.append(header)
-
-    if _tex:
-        _dummyhead = '```{%s .numberLines numbers="left"}'%(_type)
-        _dummytail = "```"
-    else:
-        _dummyhead = "```{%s}"%(_type)
-        _dummyhead = "|" + _dummyhead.ljust(_maxwidth) + "|"
-        _dummytail = "|" + "```".ljust(_maxwidth) + "|"
-
-    lines = []
-    for i in range(height):
-        swap = _read[i].rstrip('\n').replace("\t","    ")
-        width = len(swap)
-        if(_tex):
-            lines.append(swap)
-        else:
-            line = "|" + swap.ljust(_maxwidth) + "|"
-            lines.append(line)
-
-    # print lines
-
     result = []
-    result.append(hline)
-    result.extend(headers)
-    result.append(hline)
-    if(_tex):
-        result.append("")
-    result.append(_dummyhead)
-    result.extend(lines)
-    result.append(_dummytail)
-    if not _tex:
+    try:
+        with open(_file, "r") as read:
+            _read = list(read)
+        # print _read
+
+        height = len(_read) #number of lines in list
+        # print height
+
+        for i in range(height):
+            swap = _read[i].rstrip('\n').replace("\t","    ")
+            width = len(swap )
+            widthlist.append( width )
+        _maxwidth = max(widthlist)
+        # print _maxwidth
+
+        hline = "+" + "".ljust(_maxwidth,"-") + "+"
+        # print hline
+
+        headers = []
+        for statement in _list:
+            # print statement
+            header = "|" + statement.ljust(_maxwidth) + "|"
+            headers.append(header)
+        # for i in range(len(_list)):
+        #     # print _list[i]
+        #     header = "|" + _list[i].ljust(_maxwidth) + "|"
+        #     headers.append(header)
+
+        if _tex:
+            _dummyhead = '```{%s .numberLines numbers="left"}'%(_type)
+            _dummytail = "```"
+        else:
+            _dummyhead = "```{%s}"%(_type)
+            _dummyhead = "|" + _dummyhead.ljust(_maxwidth) + "|"
+            _dummytail = "|" + "```".ljust(_maxwidth) + "|"
+
+        lines = []
+        for i in range(height):
+            swap = _read[i].rstrip('\n').replace("\t","    ")
+            width = len(swap)
+            if(_tex):
+                lines.append(swap)
+            else:
+                line = "|" + swap.ljust(_maxwidth) + "|"
+                lines.append(line)
+
+        # print lines
+
         result.append(hline)
-    # print "\n".join(result)
+        result.extend(headers)
+        result.append(hline)
+        if(_tex):
+            result.append("")
+        result.append(_dummyhead)
+        result.extend(lines)
+        result.append(_dummytail)
+        if not _tex:
+            result.append(hline)
+        # print "\n".join(result)
+    except:
+        result.append("failed to open file %s" %(_file))
 
     return "\n".join(result)
 
