60d6cc5110ded2f9651135c0148b54d008fa9dd8
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-09-06 23:53:55 +0900

    add pandocker-version command to get hash of python package list

--- pandoc_misc/__init__.py
+++ pandoc_misc/__init__.py
@@ -3,11 +3,18 @@
 
 import subprocess as sp
 import site
+import pip
+import hashlib
 
 install_base = site.getsitepackages()[0].split("lib")[0][:-1]
 
 COMMAND = "docker run -v $PWD:/workdir k4zuki/pandocker make init -f {}/var/pandoc_misc/Makefile"
 command = COMMAND.format(install_base)
+env_hash = hashlib.sha1("\n".join(str(d) for d in pip.get_installed_distributions()).encode("utf-8")).hexdigest()[:7]
+
+
+def env_version():
+    print(env_hash)
 
 
 def pip_base():
