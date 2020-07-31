c41fa4e4939e3790494798643b9c44734a8e2fab
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2020-07-31 22:04:52 +0900

    update __init__.py
    - use CAPITALS for constants
    - add build_command()
    - refactor pip_base() to use installation directory of this package.
      * setuptools might be installed via system package manager (apt/apk/rpm/yum etc.).

--- pandoc_misc/__init__.py
+++ pandoc_misc/__init__.py
@@ -3,31 +3,41 @@
 
 import platform
 import subprocess as sp
-import setuptools
 import hashlib
-
-separator = "/lib/"
-docker = ""
-if platform.system() == "Windows":
-    separator = "/Lib/"
-    docker = "docker run -v $PWD:/workdir k4zuki/pandocker-alpine:2.8"
-install_base = setuptools.__path__[0].split(separator)[0]
+import yaml
 
 COMMAND = "{} make init -f {}/var/pandoc_misc/user/Makefile"
-command = COMMAND.format(docker, install_base)
+SEPARATOR = "/lib/"
+DOCKER = ""
 
-pip_list = sp.run(["pip", "list"], stdout=sp.PIPE, stderr=sp.DEVNULL)
+if platform.system() == "Windows":
+    SEPARATOR = "/Lib/"
+    DOCKER = "docker run -v $PWD:/workdir k4zuki/pandocker-alpine:2.10"
+
+
+def build_command():
+    install_base = pip_base()
+    command = COMMAND.format(DOCKER, install_base)
+
+    return command
 
 
 def pandocker_version():
+    pip_list = sp.run(["pip3", "list"], stdout=sp.PIPE, stderr=sp.DEVNULL)
     print(hashlib.sha1(pip_list.stdout).hexdigest()[:7])
 
 
 def pip_base():
-    print(install_base)
+    pip_info = sp.run(["pip3", "show", "pandoc-misc"], stdout=sp.PIPE, stderr=sp.DEVNULL)
+    pip_inst_dir = yaml.load(pip_info.stdout, Loader=yaml.SafeLoader)["Location"]
+    install_base = pip_inst_dir.split(SEPARATOR)[0]
+
+    # print(install_base, file=sys.stderr)
+    return install_base
 
 
 def main():
+    command = build_command()
     sp.call(command, shell=True)
 
 
